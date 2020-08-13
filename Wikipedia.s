610d29d5ffd018695dc60fa05011451ac13f8a20
weilin <weilin@student.42.fr>
2020-08-13 11:50:01 +0200

    Wikipedia.s

---
+++ Wikipedia.s
@@ -0,0 +1,19 @@
+.name		"Wikipedia"
+.comment	"Hello World!!!"
+
+		live	%1				#1
+		ld		%2,r3			#2
+		st		r4,5			#3
+		add		r6,r7,r8		#4
+		sub		r9,r10,r11		#5
+		and		r12,%13,r14		#6
+		or		r15,r16,r1		#7
+		xor		r2,%3,r4		#8
+		zjmp	%5				#9
+		ldi		%6,%7,r8		#10
+		sti		r9,%10,r11		#11
+		fork	%12				#12
+		lld		%13,r14			#13
+		lldi	%15,%16,r1		#14
+		lfork	%2				#15
+		aff		r3				#16
